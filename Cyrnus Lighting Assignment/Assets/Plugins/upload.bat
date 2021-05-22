00fc45b8acb57e5e4f74f6722064282c9527c2ab
cyrnus <john@cyrnus.com>
2021-05-22 16:12:36 -0400

    Added light probes and an animated sphere. Also added bulbs to the lights

--- Cyrnus Lighting Assignment/Assets/Plugins/upload.bat
+++ Cyrnus Lighting Assignment/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@U:\WIP\Unity\Altspace templates\AltspaceVR World Building\LightingAssignment\Cyrnus Lighting Assignment\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1742554256700867361.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@U:\WIP\Unity\Altspace templates\AltspaceVR World Building\LightingAssignment\Cyrnus Lighting Assignment\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1742597204754825247.json
